c0ada72d4b4b976d9605651b87037bc034207b19
Martevol <Mart@MarMac.local>
2017-05-12 14:14:04 +0700

    fix random number func

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,17 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var plus: UITextField!
+    @IBOutlet weak var equal: UITextField!
+    
     @IBOutlet weak var labelNum1: UILabel!
     @IBOutlet weak var labelNum2: UILabel!
     @IBOutlet weak var labelNum3: UILabel!
+    
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var progressBAr: UIProgressView!
-    @IBOutlet weak var test: UIButton!
     
     var time = Timer()
     var num1 = 1
     var num2 = 2
-    var num3 = 3
+    var numA = 3
+    var numB = 3
+    var answer = 0
     
     
     override func viewDidLoad() {
@@ -28,24 +33,37 @@ class ViewController: UIViewController {
         
         progressBAr.progress = 0.0
         
-        time = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+        time = Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(ViewController.tinhtoan), userInfo: nil, repeats: true)
         
-        
-
-    }
-
-    @IBAction func testRandom(_ sender: Any) {
         tinhtoan()
     }
 
+//    @IBAction func testRandom(_ sender: Any) {
+//        tinhtoan()
+//    }
+
     func tinhtoan(){
-        num1 = Int(arc4random_uniform(6))
-        num2 = Int(arc4random_uniform(6))
-        num3 = Int(arc4random_uniform(6))
+        num1 = Int(arc4random_uniform(9))
+        num2 = Int(arc4random_uniform(9))
+        
+        answer = num1 + num2
+        numA = answer - 1
+        numB = answer + 1
+        
         labelNum1.text = String(num1)
         labelNum2.text = String(num2)
-        labelNum3.text = String(num3)
+        labelNum3.text = String(randomIntFrom(start: numA, to: numB))
 
+    }
+    
+    func randomIntFrom(start: Int, to end: Int) -> Int {
+        var a = start
+        var b = end
+        // swap to prevent negative integer crashes
+        if a > b {
+            swap(&a, &b)
+        }
+        return Int(arc4random_uniform(UInt32(b - a + 1))) + a
     }
     
     func updateTimer(){
