cc7052fc2121a797906eb15a716c034ce93b05fe
Martevol <Mart@MarMac.local>
2017-05-12 12:54:39 +0700

    Random number

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelNum1: UILabel!
+    @IBOutlet weak var labelNum2: UILabel!
+    @IBOutlet weak var labelNum3: UILabel!
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var progressBAr: UIProgressView!
+    @IBOutlet weak var test: UIButton!
     
     var time = Timer()
+    var num1 = 1
+    var num2 = 2
+    var num3 = 3
     
     
     override func viewDidLoad() {
@@ -22,10 +29,24 @@ class ViewController: UIViewController {
         progressBAr.progress = 0.0
         
         time = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+        
+        
 
     }
 
+    @IBAction func testRandom(_ sender: Any) {
+        tinhtoan()
+    }
 
+    func tinhtoan(){
+        num1 = Int(arc4random_uniform(6))
+        num2 = Int(arc4random_uniform(6))
+        num3 = Int(arc4random_uniform(6))
+        labelNum1.text = String(num1)
+        labelNum2.text = String(num2)
+        labelNum3.text = String(num3)
+
+    }
     
     func updateTimer(){
         progressBAr.progress += 0.1
